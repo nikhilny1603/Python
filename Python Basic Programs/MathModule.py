7b7691426b0f36281143acfb724a8773e0a99201
Nikhil Yadav <159103944+nikhilny1603@users.noreply.github.com>
2025-02-21 16:24:16 +0530

    Add files via upload

---
+++ Python Basic Programs/MathModule.py
@@ -0,0 +1,20 @@
+import math
+
+x = 10.2
+print(math.ceil(x))
+
+y = -123
+print(math.fabs(y))
+
+z = 6
+print(math.factorial(z))
+
+a = 10.9
+print(math.floor(a))
+
+l = [1,2,3,4,5,6,7,8,9,10]
+print(math.fsum(l))
+
+print(math.sqrt(x))
+print(math.sqrt(64))
+ 
