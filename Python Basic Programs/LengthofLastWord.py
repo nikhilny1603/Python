13d08f0662723f24ba87df419a5344fcd7cf41da
Nikhil Yadav <159103944+nikhilny1603@users.noreply.github.com>
2025-02-27 18:50:36 +0530

    Add files via upload

---
+++ Python Basic Programs/LengthofLastWord.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = ""
+        for i in str:
+            newStr = str[-1:]
+            length = len(newStr)
+        return length
+    
+Solution().lengthOfLastWord("Hello World")
